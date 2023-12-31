7ca0011a523b38993e37cdda048b9f156e607cb4
harihargithub <nharihar1@gmail.com>
2023-12-31 13:27:47 +0530

    311223 1327 spinner modified

--- src/index.js
+++ src/index.js
@@ -1,40 +1,97 @@
+import React, { useEffect, useRef } from 'react';
 import { render } from 'react-dom';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Group, Sort, Inject } from '@syncfusion/ej2-react-grids';
+import { createSpinner, showSpinner, hideSpinner, setSpinner } from '@syncfusion/ej2-react-popups';
+import axios from 'axios';
 import './index.css';
-import { getValue } from '@syncfusion/ej2-base';
-import * as React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Inject } from '@syncfusion/ej2-react-grids';
-import { data } from './data';
-import { SampleBase } from './sample-base';
-import './index.css';
-export class Localbinding extends SampleBase {
-  rowDataBound(args) {
-      if (args.row) {
-        if (getValue('Freight', args.data) < 30){
-          args.row.classList.add('below-30');
-        } else if(getValue('Freight', args.data) < 80 ) {
-            args.row.classList.add('below-80');
-        } else {
-            args.row.classList.add('above-80');
-        }
-      }
-  }
-    render() {
-        return (<div className='control-pane'>
-                <div className='control-section'>
-                    <GridComponent dataSource={data} allowPaging={true} rowDataBound={this.rowDataBound} pageSettings={{ pageCount: 5 }} >
-                        <ColumnsDirective>
-                            <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign='Right'></ColumnDirective>
-                            <ColumnDirective field='CustomerName' headerText='Customer Name' width='150'></ColumnDirective>
-                            <ColumnDirective field='OrderDate' headerText='Order Date' width='130' format='yMd' textAlign='Right'/>
-                            <ColumnDirective field='Freight' headerText='Freight' width='120' format='C2' textAlign='Right'/>
-                            <ColumnDirective field='ShippedDate' headerText='Shipped Date' width='130' format='yMd' textAlign='Right'></ColumnDirective>
-                            <ColumnDirective field='ShipCountry' headerText='Ship Country' width='150'></ColumnDirective>
-                        </ColumnsDirective>
-                        <Inject services={[Page]}/>
-                    </GridComponent>
-                </div>
-            </div>);
-    }
-}
+import { Data } from './data';
 
-render(<Localbinding />, document.getElementById('sample'));
+const OrderService = {
+  BASE_URL: 'https://js.syncfusion.com/demos/ejServices/Wcf/Northwind.svc/Orders',
+
+  execute(state) {
+    return this.getData(state);
+  },
+
+  getData(state) {
+    const { skip, take, sorted } = state;
+    const pageQuery = `$skip=${skip}&$top=${take}`;
+    const sortQuery = sorted && sorted.length ? `&$orderby=` + sorted.map(obj => `${obj.name} ${obj.direction}`).join(',') : '';
+    const url = `${this.BASE_URL}?${pageQuery}${sortQuery}&$inlinecount=allpages&$format=json`;
+
+    return axios.get(url)
+      .then(response => {
+        const data = response.data;
+        return { result: data['d']['results'], count: parseInt(data['d']['__count'], 10) };
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        return { result: [], count: 0 };
+      });
+  }
+};
+
+const CustomBinding = () => {
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    const state = { skip: 0, take: 150 };
+    dataStateChange(state);
+  }, []);
+
+  const created = () => {
+    const spinnerTarget = document.querySelector('#customSpinner');
+    setSpinner({ template: '<div style="width:100%;height:100%" class="custom-rolling"><div></div></div>' }, spinnerTarget);
+    showSpinner(spinnerTarget);
+  };
+
+  const dataStateChange = (state) => {
+    const spinnerTarget = document.querySelector('#customSpinner');
+  
+    showSpinner(spinnerTarget); // Show the spinner before fetching data
+  
+    OrderService.execute(state)
+      .then(gridData => {
+        console.log('Data received:', gridData);
+        gridRef.current.dataSource = gridData;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+      .finally(() => {
+        // Delay hiding the spinner by 2 seconds to ensure it is shown
+        setTimeout(() => {
+          hideSpinner(spinnerTarget); // Hide the spinner regardless of success or failure
+        }, 2000);
+      });
+  };
+
+  return (
+    <div className='control-pane'>
+      <div id='customSpinner'></div>
+      <div className='control-section'>
+        <GridComponent
+          dataSource={[]}
+          ref={gridRef}
+          allowPaging={true}
+          allowSorting={true}
+          pageSettings={{ pageCount: 4, pageSize: 150 }}
+          height='400'
+          created={created}
+          allowGrouping={true}
+          dataStateChange={dataStateChange}
+        >
+          <ColumnsDirective>
+            <ColumnDirective field='OrderID' headerText='Order ID' width='120'></ColumnDirective>
+            <ColumnDirective field='CustomerID' headerText='Customer Name' width='150'></ColumnDirective>
+            <ColumnDirective field='ShipName' headerText='Ship Name' width='120' />
+            <ColumnDirective field='ShipCity' headerText='Ship City' width='150'></ColumnDirective>
+          </ColumnsDirective>
+          <Inject services={[Page, Group, Sort]} />
+        </GridComponent>
+      </div>
+    </div>
+  );
+};
+
+render(<CustomBinding />, document.getElementById('sample'));
---
+++ src/indexCellCondStyling.js
@@ -0,0 +1,39 @@
+// indexCellCondStyling.js
+import { render } from 'react-dom';
+import './index.css';
+import * as React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-grids';
+import { orderDetails } from './dataCellCondStyling';
+import { SampleBase } from './sample-base';
+export class Default extends SampleBase {
+
+  queryCellInfo(args) {
+    if (args.column.field == 'OrderID') {
+      if (args.data.OrderID % 2 == 0) { //based on condition we have set the font color to the cell 
+        args.cell.style.color = 'red';
+      }
+      else {
+        args.cell.style.color = 'blue';
+      }
+    }
+  }
+
+    render() {
+        return (<div className='control-pane'>
+        <div className='control-section'>
+          <GridComponent dataSource={orderDetails} height='350' queryCellInfo={this.queryCellInfo.bind(this)}>
+            <ColumnsDirective>
+              <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign='Right'></ColumnDirective>
+              <ColumnDirective field='CustomerName' headerText='Customer Name' width='150'></ColumnDirective>
+              <ColumnDirective field='OrderDate' headerText='Order Date' width='130' format='yMd' textAlign='Right'/>
+              <ColumnDirective field='Freight' headerText='Freight' width='120' format='C2' textAlign='Right'/>
+              <ColumnDirective field='ShippedDate' headerText='Shipped Date' width='130' format='yMd' textAlign='Right'></ColumnDirective>
+              <ColumnDirective field='ShipCountry' headerText='Ship Country' width='150'></ColumnDirective>
+            </ColumnsDirective>
+          </GridComponent>
+        </div>
+      </div>);
+    }
+}
+
+render(<Default />, document.getElementById('sample'));
